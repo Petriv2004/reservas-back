1436cdd960282700a49753ecb56ba1191491f779
JulianNova2004 <jnovatorroledo@gmail.com>
2024-11-16 14:06:33 -0500

    Fix: Corrected timezone

--- src/main/java/Los_Jsons/sistemas_reservas/SistemasReservasApplication.java
+++ src/main/java/Los_Jsons/sistemas_reservas/SistemasReservasApplication.java
@@ -3,10 +3,13 @@ package Los_Jsons.sistemas_reservas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SistemasReservasApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
 		SpringApplication.run(SistemasReservasApplication.class, args);
 	}
 
